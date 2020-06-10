c15ee5c6e13570096db62cd4ac1c6294e3b2abf4
asiuzzaman006 <zaman1304006@gmail.com>
2020-06-11 01:20:41 +0600

    Launching app with just two activity

--- app/src/main/java/com/example/chatapplication/LoginActivity.java
+++ app/src/main/java/com/example/chatapplication/LoginActivity.java
@@ -4,16 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
+    private Button loginButton,phoneLoginButton;
+    private EditText UserEmail,UserPassword;
+    private TextView NeedNewAccountLink,ForgetPasswordLink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        InitializeFields();
+        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToTheRegisterActivity();
+            }
+        });
+    }
+
+    private void InitializeFields() {
+        loginButton=findViewById(R.id.login_button);
+        phoneLoginButton=findViewById(R.id.phone_login_button);
+        UserEmail=findViewById(R.id.login_email);
+        UserPassword=findViewById(R.id.login_password);
+        NeedNewAccountLink=findViewById(R.id.login_using);
+        ForgetPasswordLink=findViewById(R.id.forget_password_link);
+
     }
 
     @Override
@@ -26,5 +50,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private void SendToTheMainActivity() {
         startActivity(new Intent(LoginActivity.this,MainActivity.class));
+    }
+    private void SendUserToTheRegisterActivity(){
+        startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
     }
 }
--- app/src/main/java/com/example/chatapplication/RegisterActivity.java
+++ app/src/main/java/com/example/chatapplication/RegisterActivity.java
@@ -2,13 +2,40 @@ package com.example.chatapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private Button registerButton;
+    private EditText UserEmail,UserPassword;
+    private TextView alreadyAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        InitializeFields();
+        alreadyAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToTheLoginActivity();
+            }
+        });
+    }
+
+    private void SendUserToTheLoginActivity() {
+        startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+    }
+
+    private void InitializeFields() {
+        registerButton=findViewById(R.id.register_button);
+        UserEmail=findViewById(R.id.register_email);
+        UserPassword=findViewById(R.id.register_password);
+        alreadyAccount=findViewById(R.id.already_have_account);
     }
 }
